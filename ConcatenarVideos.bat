3b4edac4bec72c19e8241c66b52826e8a059e662
Oz Ruffinelli <ozkrpy@gmail.com>
2019-07-11 17:48:11 -0400

    cambiar el tamaño del video

--- ConcatenarVideos.bat
+++ ConcatenarVideos.bat
@@ -1,5 +1,5 @@
 @echo INICIO_PROCESO
 (for %%i in (*.mp4) do @echo file '%%i') > mylist.txt
-ffmpeg -f concat -i mylist.txt -c copy output.mp4
+ffmpeg -f concat -i mylist.txt -vf scale=1280:720 -preset fast output.mp4
 del mylist.txt
 @echo FINALIZO_PROCESO
