f0870c9a3a8d0b5af02b87905eb3508f0ccab549
Oz Ruffinelli <ozkrpy@gmail.com>
2019-07-21 18:33:28 -0400

    se agrego el log del proceso de concatenacion, para inicio-fin

--- ConcatenarVideos.bat
+++ ConcatenarVideos.bat
@@ -1,5 +1,12 @@
-@echo INICIO_PROCESO
+@echo off
+set LOGFILE=batch.log
+call :LOG > %LOGFILE%
+exit /B
+
+:LOG
+
+@echo %DATE% %TIME% INICIO_PROCESO
 (for %%i in (*.mp4) do @echo file '%%i') > mylist.txt
 ffmpeg -f concat -i mylist.txt -vf scale=1280:720 -preset fast output.mp4
 del mylist.txt
-@echo FINALIZO_PROCESO
+@echo %DATE% %TIME% FINALIZO_PROCESO
