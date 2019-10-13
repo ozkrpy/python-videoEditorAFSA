91ac3e45311bdd69ce80a6dc17bbff55bfc9069e
ozkrpy <ozkrpy@gmail.com>
2019-10-13 14:38:15 -0300

    correcion de la app

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ from bottle import post, request, route, run, static_file
 # PARAMETRICO
 extension = '.mp4'
 # pathAFSA = 'C:\\Users\\ruffineo\\Desktop\\TEMPORAL-PCSISOP\\Multimedia\\AFSA\\'
-pathAFSA = './'
-#pathAFSA = 'C:\\Users\\ozkrp\\Desktop\\FORMATEO\\AFSA\\'
+pathAFSA = 'C:\\Users\\ozkrp\\Desktop\\FORMATEO\\AFSA\\'
 pathGoles = pathAFSA + 'Goles\\'
 pathTemp = pathGoles + 'temporary.mp4'
 
