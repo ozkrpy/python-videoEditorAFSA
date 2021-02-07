ae2de56e0db2bf0f4c1787538e3cb92f94d9dc35
Oz Ruffinelli <ozkrpy@gmail.com>
2021-02-06 23:40:02 -0300

    mejora en la vista html

--- forms.py
+++ forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, SelectField, IntegerField
-from wtforms.validators import DataRequired, InputRequired
+from wtforms import StringField, SubmitField, SelectField, IntegerField, DecimalField
+from wtforms.validators import DataRequired, InputRequired, ValidationError, NumberRange
 from utilities import listarPartidos
 
 # class AgregarJugadorForm(FlaskForm):
@@ -9,6 +9,10 @@ from utilities import listarPartidos
 #     numero = StringField('# Camiseta')
 #     submit = SubmitField('Aceptar')
 
+def is_42(form, field):
+    if field.data != 42:
+        raise ValidationError('Must be 42')
+
 class PartidoForm(FlaskForm):
     inicio_hora = IntegerField('INICIO')
     inicio_minuto = IntegerField()
@@ -16,13 +20,13 @@ class PartidoForm(FlaskForm):
     fin_hora = IntegerField('FINAL')
     fin_minuto = IntegerField()
     fin_segundo = IntegerField()
-    submit = SubmitField()
+    submit = SubmitField('Partido')
 
 class DestacadoForm(FlaskForm):
     partido = SelectField('Partido', choices=listarPartidos(), validators={DataRequired()})
     minuto = StringField('Minuto')
     segundo = StringField('Segundos')
-    submit = SubmitField('Cortar')
+    submit = SubmitField('Destacado')
 
 # class SortearForm(FlaskForm):
 #     lista = []
